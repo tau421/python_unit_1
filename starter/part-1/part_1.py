8bcae79bf94c42dd25752a3d01e3f4a4d34e5b08
Kyle Wilson <tau421@gmail.com>
2023-01-15 20:57:33 -0600

    initial commit

---
+++ starter/part-1/part_1.py
@@ -0,0 +1,43 @@
+# Let's assign variables to represent a book you enjoy.
+
+# Step 2 - Strings
+# Let's start with Python strings. Replace the "None" with the name of a book, and below the name of the author of your book.
+
+book_name = 'Green Eggs and Ham'
+author = 'Dr. Seuss'
+# book_name = "Dune"
+# author = "Frank Herbert"
+
+
+# Step 3 - Concatenation and f-strings
+# Use concatenation and f-strings to make a sentence about your author and book name.
+
+sentence1 = book_name + " is written by " + author + "."
+# sentence1 = book_name + " is written by " + author + "."
+sentence2 = f"{author} wrote {book_name}."
+# sentence2 = f"Author, {author}, wrote the book {book_name}."
+
+
+# Step 4 - Integers
+# Now let's practice Integers. Replace the "None" below with the publication date of your book.
+publication_year = 1960
+# publication_year = 1965
+
+
+# Step 5 - Floats
+# Now estimate what the retail value of your book would be at a store and replace the "None" below with a float value of the price. (Doesn't have to be accurate.)
+book_price = 6.99
+# book_price = 17.99
+
+
+# Step 6 - Booleans
+# Now we will practice with booleans. Replace the "is_awesome" variable with a boolean. True if your book is awesome, False if your book is not awesome.
+is_awesome = True
+# is_awesome = True
+
+
+# Step 7 - print and type functions
+# Follow the instructions, and below this line, code all of the suggested print statements and the type statements.
+# Code below
+print(book_name, author, sentence1, sentence2, publication_year, book_price, is_awesome)
+print(type(book_name))
